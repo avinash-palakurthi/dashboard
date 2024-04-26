7f7de473a9a7c85060f355ccae86b7b17970c53c
Avinash <palakurthiavinash07@gmail.com>
2024-04-26 18:16:00 +0530

    project and router setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+	return (
+		<>
+			<h1>Welcome to Dashboard </h1>
+			<Button className="ml-2 px-14">ok</Button>
+		</>
+	);
+}
+
+export default App;
