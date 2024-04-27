cb4323f004d43b6a4ad5b11f2b67465dcce2811e
Avinash <palakurthiavinash07@gmail.com>
2024-04-27 12:33:20 +0530

    added auth layout

--- src/router.tsx
+++ src/router.tsx
@@ -2,20 +2,38 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
-
+import DashboardLayout from "./layouts/DashboardLayout";
+import BooksPage from "./pages/BooksPage";
+import AuthLayout from "./layouts/AuthLayout";
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <HomePage />,
+		path: "dashboard",
+		element: <DashboardLayout />,
+		children: [
+			{
+				path: "home",
+				element: <HomePage />,
+			},
+			{
+				path: "books",
+				element: <BooksPage />,
+			},
+		],
 	},
+	{
+		path: "/auth",
+		element: <AuthLayout />,
 
-	{
-		path: "/login",
-		element: <LoginPage />,
-	},
-	{
-		path: "/register",
-		element: <RegisterPage />,
+		children: [
+			{
+				path: "login",
+				element: <LoginPage />,
+			},
+			{
+				path: "register",
+				element: <RegisterPage />,
+			},
+		],
 	},
 ]);
 
